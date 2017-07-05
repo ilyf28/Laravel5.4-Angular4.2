e36609a867af2d1c2aa3fd37235153ddbe0935ea
Suhyun Lee <ilyf28@gmail.com>
2017-07-05 09:04:32 +0000

    Modify public/js/*.js file not found error

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,21 @@ const elixir = require('laravel-elixir');
 const gulp = require('gulp');
 const gulpWebpack = require('webpack-stream');
 const named = require('vinyl-named');
+const Task = elixir.Task;
 
-gulp.task('typescript', function() {
-    return gulp
-        .src(['resources/assets/typescript/**/*.ts'])
-        .pipe(named())
-        .pipe(gulpWebpack(require('./webpack.config.js')))
-        .pipe(gulp.dest('public/js'));
+elixir.extend('typescript', function() {
+    new Task('typescript', function() {
+        return gulp
+            .src(['resources/assets/typescript/**/*.ts'])
+            .pipe(named())
+            .pipe(gulpWebpack(require('./webpack.config.js')))
+            .pipe(gulp.dest('public/js'));
+    });
 });
 
 elixir(function(mix) {
     mix.sass('app.scss');
-    mix.task('typescript');
+    mix.typescript();
     mix.version([
         'public/js/main.js',
         'public/js/polyfill.js'
